b39721c6da842871388fcc4ee90de703072cfd68
psantisteban <psantisteban@autonoma.edu.pe>
2025-07-09 07:49:09 +0000

    transformacion de "usuarios"

---
+++ transformacion/procesos/dim_usuarios.py
@@ -0,0 +1,31 @@
+import pandas as pd
+from io import StringIO
+
+# --- Carga de datos ---
+usuarios_csv = '../../extraccion/colecciones/usuarios.csv'
+df = pd.read_csv(usuarios_csv)
+
+# --- Transformación de datos ---
+# remplaza id por id_origen para mantener las referencias del id original 
+df = df.rename(columns={'id': 'id_origen'})
+
+# generar id secuenciales de 1 a n
+df.insert(0, 'id', range(1, 1 + len(df)))
+
+# renombrar 'fechaCreacion' a 'fecha_creacion'
+df = df.rename(columns={'fechaCreacion': 'fecha_creacion'})
+
+# reordenar las columnas 
+df = df[['id', 'nombre', 'dni', 'area', 'empresa', 'fecha_creacion', 'id_origen']]
+
+# transforma dni de texto a entero
+df['dni'] = df['dni'].astype(int)
+
+# transforma la fecha a formato datetime
+df['fecha_creacion'] = pd.to_datetime(df['fecha_creacion'])
+
+# --- Guardar el resultado ---
+usuarios_csv_salida = '../resultados/dim_usuario.csv'
+df.to_csv(usuarios_csv_salida, index=False)
+
+print(f"Datos transformados y guardados en '{usuarios_csv_salida}'")
