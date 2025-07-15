4acccc594a9a2c6dc4419d924749223fc09972c7
psantisteban <psantisteban@autonoma.edu.pe>
2025-07-14 21:26:48 -0500

    correccion de conexion a la db

--- procesos/carga/carga.py
+++ procesos/carga/carga.py
@@ -1,9 +1,11 @@
 from db import db_mysql
 from . import dim_usuario_carga, dim_dispositivo_carga, fact_inactividad_carga, fact_reserva_carga, fact_sesion_carga, fact_ubicacion_gps_carga
 
+db, cursor = db_mysql.iniciar_conexion()
+
 def borrar_tablas():
     print("Borrando tablas...")
-    db, cursor = db_mysql.iniciar_conexion()
+    
     tablas = ['fact_ubicacion_gps', 'fact_sesion', 'fact_reserva', 'fact_inactividad', 'dim_dispositivo', 'dim_usuario']
     for tab in tablas:
         try:
@@ -13,7 +15,6 @@ def borrar_tablas():
             print(f"Tabla {tab} borrada")
         except :
             print(f"La tabla {tab} no existe")
-    db_mysql.finalizar_conexion(db, cursor)
     print("Todas las tablas borradas")
 
 def crear_tablas():
@@ -21,17 +22,14 @@ def crear_tablas():
     sql = open("sql/tablas.sql", "r")
     texto_sql = sql.read()
     sentencias_sql = texto_sql.split(";")
-    db, cursor = db_mysql.iniciar_conexion()
     for sentencia in sentencias_sql:
         cursor.execute(sentencia)
         db.commit()
-    db_mysql.finalizar_conexion(db, cursor)
     print("Todas las tablas han sido creadas")
 
 def iniciar(df_usuario, df_dispositivo, df_inactividad, df_reserva, df_sesion, df_ubicacion):
     print("\nCargando datos a la base de datos")
     
-    db, cursor = db_mysql.iniciar_conexion()
     dim_usuario_carga.cargar(df_usuario, db, cursor)
     dim_dispositivo_carga.cargar(df_dispositivo, db, cursor)
     fact_inactividad_carga.cargar(df_inactividad, db, cursor)
